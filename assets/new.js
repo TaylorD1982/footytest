ea52827b0bf156cafd63abb1b958abba4f82eed7
Ubuntu <ubuntu@ip-172-31-91-149.ec2.internal>
2019-08-02 10:26:11 +0000

    first commit

---
+++ assets/new.js
@@ -0,0 +1,4 @@
+var cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars[1];
+document.getElementById("demo1").innerHTML = cars[2];
+document.getElementById("demo2").innerHTML = cars[2];
